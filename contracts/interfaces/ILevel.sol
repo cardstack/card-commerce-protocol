6e6f468e00c8a427562d545bd27b961732216a16
Justin <justinthong93@gmail.com>
2021-07-18 23:16:17 +0800

    update interface

--- contracts/interfaces/ILevel.sol
+++ contracts/interfaces/ILevel.sol
@@ -6,13 +6,24 @@ interface ILevel {
         uint256 startBlock;
     }
 
+    struct Level {
+        string label;
+    }
+
     event RootSubmission(bytes32 beneficiary, uint256 cycle);
     event BeneficiaryClaimLevel(address indexed beneficiary);
+    event LevelCreated(address creator);
+    event LevelBadgeMinted();
+    event CrossHonorCreated(address creator);
     event CycleEnded(uint256 number, uint256 startBlock, uint256 endBlock);
 
     function writeRoot(bytes32 proof) external;
 
-    // function claimLevel(address beneficiary) external;
+    function claimLevel(address beneficiary) external;
+
+    function createLevel() external;
+
+    function crossHonorLevel() external;
 
     // function levelForProof(address beneficiary, bytes calldata proof) external;
 

d147b038d8b56ba1fb06f19cfd80e1b28f4dc1b2
Justin <justinthong93@gmail.com>
2021-07-21 11:59:14 +0800

    remove proof logic interface

--- contracts/interfaces/ILevel.sol
+++ contracts/interfaces/ILevel.sol
@@ -2,34 +2,17 @@ pragma solidity 0.6.8;
 pragma experimental ABIEncoderV2;
 
 interface ILevel {
-    struct Cycle {
-        uint256 number;
-        uint256 startBlock;
-    }
-
-    // struct Level {
-    //     string label;
-    //     Badge badge;
-    // }
-
     struct Badge {
         address token;
         uint256 tokenID;
     }
 
-    event RootSubmission(bytes32 beneficiary, uint256 cycle);
-    event BeneficiaryClaimLevel(address indexed beneficiary);
     event LevelCreated(address creator);
     event LevelSet(address badgeAddress, address beneficiary);
-    event LevelBadgeMinted();
+    event LevelBadgeMinted(address badgeAddress);
     event CrossHonorCreated(address creator);
 
-    // event CycleEnded(uint256 number, uint256 startBlock, uint256 endBlock);
-
-    // function writeRoot(bytes32 root) external;
-
-    // function claimLevel(bytes calldata proof) external;
-
+    //Level
     function createLevel(address badge) external;
 
     function setLevel(
@@ -40,21 +23,9 @@ interface ILevel {
 
     function crossHonorLevel() external;
 
-    // function getLevels(address beneficiary)
-    //     external
-    //     view
-    //     returns (Level memory);
-
+    //View
     function hasLevel(address badge, address beneficiary)
         external
         view
         returns (bool);
-
-    // function checkLevel(address beneficiary) external view;
-
-    // function levelForProof(address beneficiary, bytes calldata proof) external;
-
-    // function verifyProof(address beneficiary, bytes calldata proof) external;
-
-    // function proofUsed(bytes calldata proof) external;
 }

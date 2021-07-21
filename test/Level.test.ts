8db167535977b492a665bc9e1fafb8b43a3048e9
Justin <justinthong93@gmail.com>
2021-07-21 14:38:17 +0800

    udpate level test

--- test/Level.test.ts
+++ test/Level.test.ts
@@ -8,12 +8,12 @@ import { BaseErc721Factory } from '../typechain/BaseErc721Factory';
 import { Level } from '../typechain/Level';
 import { BaseErc20, BaseErc721 } from '../typechain';
 import { BytesLike, BigNumber, Wallet } from 'ethers';
-import { randomBytes } from 'ethers/lib/utils';
 import { ethers } from 'ethers';
 import { LevelFactory } from '../typechain/LevelFactory';
 import { Erc721Mintable } from '../typechain/Erc721Mintable';
 import { Erc721MintableFactory } from '../typechain/Erc721MintableFactory';
 import { MaxUint256, AddressZero } from '@ethersproject/constants';
+import { beforeEach } from 'mocha';
 
 chai.use(asPromised);
 let provider = new JsonRpcProvider();
@@ -55,15 +55,9 @@ describe('Level Registrar 2', () => {
   async function tokenAs(token: string, wallet: Wallet) {
     return BaseErc721Factory.connect(token, wallet);
   }
-  sampleRoot =
-    '0x1db9340101379dc2dfc7cc11f178e9b02489d10700ea911dffa7adf45ebef56f';
-
-  sampleProof =
-    '0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000008AC7230489E8000066acfc5050b10b37812fc2a5902d9ba6ba8e0b057e8524b6de86e9da28b03b59b2eeddc38a59a9c5d0f5cd72d18afbb864da3e3d1b6ceb520c42d9be7206f7ab74cba4ec1e5de6222071c2237d42c316c664ec4dbd3fc8d195de6419e96ca2f5';
-  sampleProofBytes = randomBytes(8);
 
   describe('#constructor', () => {
-    it('should be able to deploy', async () => {
+    it('Should be able to deploy', async () => {
       await expect(deploy()).eventually.fulfilled;
     });
   });
@@ -96,8 +90,11 @@ describe('Level Registrar 2', () => {
     it('Can add badge', async () => {
       await levelRegistrarContract.createLevel(noob.address);
     });
+    it('Can remove badge', async () => {
+      await levelRegistrarContract.removeLevel(noob.address);
+    });
 
-    it('Cannot set level if badge not added', async () => {
+    it('Cannot set level if badge not added / has been removed', async () => {
       await expect(
         levelRegistrarContract.setLevel(
           noobContract.address,
@@ -118,41 +115,65 @@ describe('Level Registrar 2', () => {
       ).rejectedWith('ERC721: transfer caller is not owner nor approved');
     });
 
-    it('Can set level', async () => {
-      await noobContract.setApprovalForAll(
-        levelRegistrarContract.address,
-        true
-      );
-      let isApproved: boolean = await noobContract.isApprovedForAll(
-        deployerWallet.address,
-        levelRegistrarContract.address
-      );
-      await expect(isApproved, 'badge contract should be approved').to.be.true;
-      let ownerOldBalance: BigNumber = await noobContract.balanceOf(
-        deployerWallet.address
-      );
-      expect(ownerOldBalance.toNumber()).eq(2);
-
-      await levelRegistrarContract.createLevel(noobContract.address);
-      await levelRegistrarContract.setLevel(
-        noobContract.address,
-        1,
-        otherWallet.address
-      );
-      let ownerNewBalance: BigNumber = await noobContract.balanceOf(
-        deployerWallet.address
-      );
-      let receiverNewBalance: BigNumber = await noobContract.balanceOf(
-        otherWallet.address
-      );
-      let contractNewBalance: BigNumber = await noobContract.balanceOf(
-        levelRegistrarContract.address
-      );
-      expect(ownerNewBalance.toNumber()).eq(1);
-      expect(receiverNewBalance.toNumber()).eq(1);
-      expect(contractNewBalance.toNumber()).eq(0);
-      let hasLevel: boolean =await levelRegistrarContract.hasLevel(noobContract.address, otherWallet.address)
-      expect(hasLevel).to.be.true
+    describe('#approve', () => {
+      beforeEach(async () => {
+        await noobContract.setApprovalForAll(
+          levelRegistrarContract.address,
+          true
+        );
+        let isApproved: boolean = await noobContract.isApprovedForAll(
+          deployerWallet.address,
+          levelRegistrarContract.address
+        );
+        await expect(isApproved, 'badge contract should be approved').to.be
+          .true;
+        let ownerOldBalance: BigNumber = await noobContract.balanceOf(
+          deployerWallet.address
+        );
+        expect(ownerOldBalance.toNumber()).eq(2);
+      });
+      it('Can set level', async () => {
+        await levelRegistrarContract.createLevel(noobContract.address);
+        await levelRegistrarContract.setLevel(
+          noobContract.address,
+          1,
+          otherWallet.address
+        );
+        let ownerNewBalance: BigNumber = await noobContract.balanceOf(
+          deployerWallet.address
+        );
+        let receiverNewBalance: BigNumber = await noobContract.balanceOf(
+          otherWallet.address
+        );
+        let contractNewBalance: BigNumber = await noobContract.balanceOf(
+          levelRegistrarContract.address
+        );
+        expect(ownerNewBalance.toNumber()).eq(1);
+        expect(receiverNewBalance.toNumber()).eq(1);
+        expect(contractNewBalance.toNumber()).eq(0);
+        let hasLevel: boolean = await levelRegistrarContract.hasLevel(
+          noobContract.address,
+          otherWallet.address
+        );
+        expect(hasLevel).to.be.true;
+      });
+      it('Can unset level', async () => {
+        await levelRegistrarContract.createLevel(noobContract.address);
+        await levelRegistrarContract.setLevel(
+          noobContract.address,
+          1,
+          otherWallet.address
+        );
+        await levelRegistrarContract.unsetLevel(
+          noobContract.address,
+          otherWallet.address
+        );
+        let hasLevel: boolean = await levelRegistrarContract.hasLevel(
+          noobContract.address,
+          otherWallet.address
+        );
+        expect(hasLevel).to.be.false;
+      });
     });
   });
 });

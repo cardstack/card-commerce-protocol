5fd0cbe861c637d4d0e6483b83c24155ecec19b3
Justin <justinthong93@gmail.com>
2021-07-21 11:59:04 +0800

    remove proof logic

--- contracts/Level.sol
+++ contracts/Level.sol
@@ -1,11 +1,7 @@
 pragma solidity 0.6.8;
 pragma experimental ABIEncoderV2;
 
-import {
-    MerkleProof
-} from "@openzeppelin/contracts/cryptography/MerkleProof.sol";
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
-import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 import {ILevel} from "./interfaces/ILevel.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
@@ -13,44 +9,21 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 contract Level is ILevel {
     using SafeMath for uint256;
     using Counters for Counters.Counter;
-    // using MerkleProof for bytes32[];
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    // ILevel.Cycle public cycle;
-    // Counters.Counter private levelID;
     address public tally;
     EnumerableSet.AddressSet private _registeredBadges;
 
     mapping(address => address) beneficiaries; //badge <> beneficiary address
-
-    // mapping(uint256 => bytes32) roots;
-    // mapping(address => bytes32) usedProofs;
 
     modifier onlyTally() {
         require(tally == msg.sender, "Caller is not tally");
         _;
     }
 
-    // function writeRoot(bytes32 root) external override {
-    //     roots[cycle.number] = root;
-    //     _startNewCycle();
-    //     emit RootSubmission(root, cycle.number);
-    // }
-
-    // claim level may be in reward contract
-    // function claimLevel(bytes calldata proof) external override {
-    //     // verify proof here
-    //     emit BeneficiaryClaimLevel(msg.sender);
-    // }
-
-    // holder of nft has to tell contract
     function createLevel(address badge) external override {
         _registeredBadges.add(badge);
         emit LevelCreated(msg.sender);
-    }
-
-    function crossHonorLevel() external override {
-        emit CrossHonorCreated(msg.sender);
     }
 
     function setLevel(
@@ -74,15 +47,12 @@ contract Level is ILevel {
         emit LevelBadgeMinted();
     }
 
-    // function getLevels(address beneficiary)
-    //     external
-    //     view
-    //     override
-    //     returns (Level memory)
-    // {
-    //     return levels[beneficiary];
-    // }
+    //TBD
+    function crossHonorLevel() external override {
+        emit CrossHonorCreated(msg.sender);
+    }
 
+    // View
     function hasLevel(address badge, address beneficiary)
         external
         view
@@ -92,12 +62,4 @@ contract Level is ILevel {
         if (beneficiaries[badge] != address(0)) return true;
         else return false;
     }
-
-    // function _startNewCycle() internal onlyTally {
-    //     require(
-    //         block.number > cycle.startBlock,
-    //         "Cannot start new payment cycle before currentPaymentCycleStartBlock"
-    //     );
-    //     emit CycleEnded(cycle.number, cycle.startBlock, block.number);
-    // }
 }
